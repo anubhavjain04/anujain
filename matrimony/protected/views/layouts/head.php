bf97bcf7a3ddcaa7c16055c29cb5b5e6ebe0a371
Anubhav Jain <anubhavjain04@gmail.com>
2013-10-05 14:02:46 +0530

    Matrimony: Make IE compatible.

--- matrimony/protected/views/layouts/head.php
+++ matrimony/protected/views/layouts/head.php
@@ -6,8 +6,33 @@
 <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/js/lib/css/kendo.default.min.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/pager.css" />
-
-<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/lib/json2.js" ></script>
+ <!--[if lt IE 9]>
+ 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie-fix.css" />
+ 	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/lib/json2.js" ></script>
+ <![endif]-->
+ <!--[if lt IE 10]>
+     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/lib/respond.min.js"></script>
+ <![endif]-->
+ <!--[if IE 10]>
+ 	<style>
+ 		@-webkit-viewport   { width: device-width; }
+		@-moz-viewport      { width: device-width; }
+		@-ms-viewport       { width: device-width; }
+		@-o-viewport        { width: device-width; }
+		@viewport           { width: device-width; }
+ 	</style>
+ 	<script>
+ 		if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+		  var msViewportStyle = document.createElement("style")
+		  msViewportStyle.appendChild(
+		    document.createTextNode(
+		      "@-ms-viewport{width:auto!important}"
+		    )
+		  )
+		  document.getElementsByTagName("head")[0].appendChild(msViewportStyle)
+		}
+ 	</script>
+ <![endif]-->
 <!-- javascript -->
 <?php
     Yii::app()->clientScript->registerScript('search', "
