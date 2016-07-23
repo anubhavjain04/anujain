fc95cc75ec67e7cea01871e84ad21614a80f8ec3
Anubhav Jain <anubhav.jain@kronos.com>
2016-07-23 19:46:52 +0530

    matrimony: google analytics

--- matrimonyNew/protected/views/layouts/main.php
+++ matrimonyNew/protected/views/layouts/main.php
@@ -20,15 +20,7 @@
   <meta name="author" content="rajkamal" />
   <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
   <title><?php echo CHtml::encode($this->pageTitle); ?></title>
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-44190584-1', 'bhartiyajainmilan.com');
-    ga('send', 'pageview');
-  </script>
+  <?php include_once("analyticstracking.php") ?>
   <?php include('head.php'); ?>
 <!--  <script type="text/javascript" src="http://www.google.com/recaptcha/api/js/recaptcha_ajax.js"></script>-->
 
